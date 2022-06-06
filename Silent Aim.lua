c769d055f4fb52c917b41616b3695a0c80db1cd0
TheSplxsh <103442236+TheSplxsh@users.noreply.github.com>
2022-06-06 17:17:28 -0400

    Update Silent Aim.lua

--- Silent Aim.lua
+++ Silent Aim.lua
@@ -2,7 +2,7 @@ if not syn or not protectgui then
     getgenv().protectgui = function()end
 end
 local Library = loadstring(game:HttpGet('https://lindseyhost.com/UI/LinoriaLib.lua'))()
--- Library:SetWatermark("github.com/TheSplxsh")
+Library:SetWatermark("github.com/TheSplxsh")
 
 local Camera = workspace.CurrentCamera
 local Players = game:GetService("Players")
